23638e4f60535e18cffb3a82eba5579727de47b1
Kalle <kh.hansson@gmail.com>
2019-10-01 14:04:21 +0200

    Fixed files in package

--- packages/webpack-simple/webpack.config.babel.js
+++ packages/webpack-simple/webpack.config.babel.js
@@ -5,6 +5,6 @@ import p from 'path';
 import { createApiConfig } from './src';
 
 const entry = p.resolve('./src/index.ts');
-const path = p.resolve('./dist');
+const path = p.resolve('./lib');
 
 export default createApiConfig({ entry, path });
