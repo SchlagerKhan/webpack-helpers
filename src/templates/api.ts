6563efb108eabd6c85ddf7a1c02956fd4cefb843
Kalle <kh.hansson@gmail.com>
2019-11-26 05:43:09 +0100

    Misc

--- src/templates/api.ts
+++ src/templates/api.ts
@@ -2,9 +2,7 @@ import { Configuration } from 'webpack';
 
 import { createNodeConfig, BasicNodeOpts } from './node';
 
-interface ApiOpts extends BasicNodeOpts {
-	entry: string;
-}
+interface ApiOpts extends BasicNodeOpts {}
 
 export function createApiConfig(apiOpts: ApiOpts, otherOpts?: Configuration): Configuration {
 	const distFile = 'index.js';
