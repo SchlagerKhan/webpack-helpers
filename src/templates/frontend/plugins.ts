8729e98f674e85c93fbf421f0a76fa8214303c2b
Kalle <kh.hansson@gmail.com>
2019-11-27 09:25:10 +0100

    Default opts for html plugin

--- src/templates/frontend/plugins.ts
+++ src/templates/frontend/plugins.ts
@@ -1,7 +1,9 @@
 import { CssPlugin, HtmlInjectPlugin, HtmlPlugin, HtmlRootPlugin } from '../../plugins';
 import { getCssFile } from '../../helpers';
 
-export function getPlugins(htmlOptions, htmlInject) {
+export function getPlugins(_htmlOptions, htmlInject) {
+	const htmlOptions = Object.assign({ title: '' }, _htmlOptions);
+
 	const plugins = [
 		// prettier-ignore
 		new CssPlugin({ filename: getCssFile() }),
